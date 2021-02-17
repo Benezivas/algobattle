42d4715b2007bb77a5923462824155c2aac5e2c1
Henri Lotze <info@henrilotze.de>
2021-02-17 14:42:36 +0100

    Add MANIFEST

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
+from glob import glob
 
 setup(
     name='algobattle',
     version='0.1.0',
     packages=['algobattle'],
     scripts=['scripts/algobattle'],
+    python_requires='>=3.9',
+    include_package_data=True
 )
