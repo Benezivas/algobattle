0eb4263baa803737194fda15b69ea83d638889b0
Henri Lotze <info@henrilotze.de>
2021-02-27 17:45:54 +0100

    Add tests for c4subgraphiso Problem

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -3,12 +3,14 @@
 import unittest
 
 import tests.test_problem_biclique as biclique
+import tests.test_problem_c4subgprahiso as c4subgraphiso
 
 def suites():
-    #suite = unittest.TestSuite()
     suites = []
     suites.append(unittest.defaultTestLoader.loadTestsFromTestCase(biclique.Parsertests))
     suites.append(unittest.defaultTestLoader.loadTestsFromTestCase(biclique.Verifiertests))
+    suites.append(unittest.defaultTestLoader.loadTestsFromTestCase(c4subgraphiso.Parsertests))
+    suites.append(unittest.defaultTestLoader.loadTestsFromTestCase(c4subgraphiso.Verifiertests))
     return suites
 
 if __name__ == '__main__':
