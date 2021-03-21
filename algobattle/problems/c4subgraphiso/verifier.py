c549e781111c6764a57335c4b671eac7b064b4f3
Henri Lotze <info@henrilotze.de>
2021-03-21 19:07:04 +0100

    Reduce complexity of verifier functions

--- algobattle/problems/c4subgraphiso/verifier.py
+++ algobattle/problems/c4subgraphiso/verifier.py
@@ -20,17 +20,20 @@ class C4subgraphisoVerifier(Verifier):
                 used_nodes.add(sol_node)
         return True
 
+    def square_in_instance(self, square, instance):
+        if (not (('e', square[0], square[1]) in instance or ('e', square[1], square[0]) in instance)
+                or not (('e', square[1], square[2]) in instance or ('e', square[2], square[1]) in instance)
+                or not (('e', square[2], square[3]) in instance or ('e', square[3], square[2]) in instance)
+                or not (('e', square[3], square[0]) in instance or ('e', square[0], square[3]) in instance)):
+            return False
+        return True
+
     def verify_solution_against_instance(self, instance, solution, instance_size, solution_type):
         solution = [line[1:] for line in solution]
         for sol_square in solution:
-            if (not (('e', sol_square[0], sol_square[1]) in instance or ('e', sol_square[1], sol_square[0]) in instance)
-                or not (('e', sol_square[1], sol_square[2]) in instance or ('e', sol_square[2], sol_square[1]) in instance)
-                or not (('e', sol_square[2], sol_square[3]) in instance or ('e', sol_square[3], sol_square[2]) in instance)
-                or not (('e', sol_square[3], sol_square[0]) in instance or ('e', sol_square[0], sol_square[3]) in instance)
-                or ('e', sol_square[0], sol_square[2]) in instance
-                or ('e', sol_square[2], sol_square[0]) in instance
-                or ('e', sol_square[1], sol_square[3]) in instance
-                or ('e', sol_square[3], sol_square[1]) in instance):
+            unwanted_edges = [('e', sol_square[0], sol_square[2]), ('e', sol_square[2], sol_square[0]),
+                              ('e', sol_square[1], sol_square[3]), ('e', sol_square[3], sol_square[1])]
+            if not self.square_in_instance(sol_square, instance) or any(edge in instance for edge in unwanted_edges):
                 logger.error('At least one element of the solution is not an induced square in the input graph!')
                 return False
         return True
