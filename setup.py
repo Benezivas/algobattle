744fc7293067bc72bbd3bc6fb4e6dddb1c5776d2
Henri Lotze <info@henrilotze.de>
2021-10-11 15:57:00 +0200

    Version string now fetched from __init__.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
+from pathlib import Path
+
+# Stolen from microsofts recommenders repository:
+here = Path(__file__).absolute().parent
+version_data = {}
+with open(here.joinpath("algobattle", "__init__.py"), "r") as f:
+    exec(f.read(), version_data)
+version = version_data.get("__version__", "0.0")
 
 setup(
     name='algobattle',
-    version='2.0.0',
+    version=version,
     packages=['algobattle'],
     scripts=['scripts/battle'],
     python_requires='>=3.6',
