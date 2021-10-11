a85c15292a076582d34226dc8e936c1d4444dcd9
Henri Lotze <info@henrilotze.de>
2021-10-11 15:38:18 +0200

    Increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='algobattle',
-    version='1.0.1',
+    version='2.0.0',
     packages=['algobattle'],
     scripts=['scripts/battle'],
     python_requires='>=3.6',
