a47972a9b6a30860396279e2a258bce2fb3390fe
Henri Lotze <info@henrilotze.de>
2022-01-14 14:19:56 +0100

    Add tests for battle_wrappers, update other test files accordingly

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,7 +7,7 @@ import os
 import algobattle
 from algobattle.match import Match
 from algobattle.team import Team
-from algobattle.util import import_problem_from_path, measure_runtime_overhead, calculate_points, run_subprocess
+from algobattle.util import import_problem_from_path, measure_runtime_overhead, run_subprocess
 
 logging.disable(logging.CRITICAL)
 
@@ -27,46 +27,6 @@ class Utiltests(unittest.TestCase):
 
     def test_measure_runtime_overhead(self):
         self.assertGreater(measure_runtime_overhead(), 0)
-
-    def test_calculate_points_weird_type(self):
-        match_data = {'rounds': 2, 'type': 'foo'}
-        self.assertEqual(calculate_points(match_data, 100), {})
-
-    def test_calculate_points_zero_rounds(self):
-        match_data = {'rounds': 0, 'type': 'iterated'}
-        self.assertEqual(calculate_points(match_data, 100), {})
-
-    def test_calculate_points_iterated_draw(self):
-        match_data = {'rounds': 2,
-                      'type': 'iterated',
-                      ('0', '1'): {0: {'solved': 20}, 1: {'solved': 10}},
-                      ('1', '0'): {0: {'solved': 10}, 1: {'solved': 20}}}
-        self.assertEqual(calculate_points(match_data, 100), {'0': 50, '1': 50})
-
-    def test_calculate_points_iterated_domination(self):
-        match_data = {'rounds': 2,
-                      'type': 'iterated',
-                      ('0', '1'): {0: {'solved': 10}, 1: {'solved': 10}},
-                      ('1', '0'): {0: {'solved': 0}, 1: {'solved': 0}}}
-        self.assertEqual(calculate_points(match_data, 100), {'0': 0, '1': 100})
-
-    def test_calculate_points_averaged_draw(self):
-        match_data = {'rounds': 2,
-                      'type': 'averaged',
-                      ('0', '1'): {0: {'approx_ratios': [1.5, 1.5, 1.5]},
-                                   1: {'approx_ratios': [1.5, 1.5, 1.5]}},
-                      ('1', '0'): {0: {'approx_ratios': [1.5, 1.5, 1.5]},
-                                   1: {'approx_ratios': [1.5, 1.5, 1.5]}}}
-        self.assertEqual(calculate_points(match_data, 100), {'0': 50, '1': 50})
-
-    def test_calculate_points_averaged_domination(self):
-        match_data = {'rounds': 2,
-                      'type': 'averaged',
-                      ('0', '1'): {0: {'approx_ratios': [1.5, 1.5, 1.5]},
-                                   1: {'approx_ratios': [1.5, 1.5, 1.5]}},
-                      ('1', '0'): {0: {'approx_ratios': [1.0, 1.0, 1.0]},
-                                   1: {'approx_ratios': [1.0, 1.0, 1.0]}}}
-        self.assertEqual(calculate_points(match_data, 100), {'0': 60, '1': 40})
 
     def test_run_subprocess(self):
         team = Team('0', self.tests_path + '/generator_timeout', self.tests_path + '/solver')
