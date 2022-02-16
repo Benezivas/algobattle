d11857cb4b140ffff40245c6113f43df7c2528cf
Imogen <me@imogen.tech>
2022-02-16 14:56:27 +0100

    Subject typehint w/o circular import

--- algobattle/observer.py
+++ algobattle/observer.py
@@ -1,11 +1,15 @@
 """Observer class for an Observable pattern."""
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from algobattle.subject import Subject
 
 
 class Observer(ABC):
     """The Observer interface declares the update method, used by subjects."""
 
     @abstractmethod
-    def update(self, subject) -> None:  # TODO: Subject typehint w/o circular import
+    def update(self, subject: Subject) -> None:
         """Receive update from subject."""
-        pass
+        raise NotImplementedError
