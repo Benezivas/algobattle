29b1f01a304647708dc9ebf9870a55af38bb1efe
Imogen <me@imogen.tech>
2022-02-19 02:16:01 +0100

    fix import

--- algobattle/team.py
+++ algobattle/team.py
@@ -1,6 +1,6 @@
 """Team class, stores necessary information about a Team, such as their associated solver and generator."""
 from __future__ import annotations
-from docker import Image
+from algobattle.docker import Image
 
 class Team:
     """Team class responsible for holding basic information of a specific team."""
