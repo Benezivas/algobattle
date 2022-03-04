4036189f0783786232d45378f7e0b80363b49079
Imogen <me@imogen.tech>
2022-03-04 03:30:04 +0100

    update test problems

--- algobattle/problems/delaytest/problem.py
+++ algobattle/problems/delaytest/problem.py
@@ -1,18 +1,38 @@
 """Dummy problem class for delaytest."""
 import logging
 
-from algobattle.problem import Problem
-from .parser import DelaytestParser
-from .verifier import DelaytestVerifier
+from algobattle.problem import ApproxType, Problem
 
 logger = logging.getLogger('algobattle.problems.delaytest')
 
 
-class Delaytest(Problem):
+class Delaytest(Problem[None, None]):
     """Dummy Problem used for testing Docker delays."""
 
-    name: str = 'Runtime Delay Test'
+    name = 'Runtime Delay Test'
     n_start: int = 1
-    parser: DelaytestParser = DelaytestParser()
-    verifier: DelaytestVerifier = DelaytestVerifier()
-    approximable: bool = False
+    approx_type = ApproxType.maximize
+
+    @staticmethod
+    def split(input: str) -> tuple[list[str], list[str]]:
+        return [], []
+    
+    @staticmethod
+    def parse_instance(instance: list[str], size: int) -> None:
+        return None
+    
+    @staticmethod
+    def parse_solution(solution: list[str], size: int) -> None:
+        return None
+    
+    @staticmethod
+    def encode_instance(instance: None) -> str:
+        return ""
+    
+    @staticmethod
+    def verify_solution(instance: None, size: int, solution: None) -> bool:
+        return True
+    
+    @staticmethod
+    def solution_weight(instance: None, size: int, solutoion: None) -> float:
+        return 1
--- algobattle/problems/testsproblem/problem.py
+++ algobattle/problems/testsproblem/problem.py
@@ -1,18 +1,61 @@
 """Problem class built for tests."""
 import logging
 
-from algobattle.problem import Problem
-from .parser import TestsParser
-from .verifier import TestsVerifier
+from algobattle.problem import Problem, ApproxType
 
 logger = logging.getLogger('algobattle.problems.testsproblem')
 
 
-class Tests(Problem):
+class Tests(Problem[list[list[str]], list[str]]):
     """Artificial problem used for tests."""
 
-    name: str = 'Tests'
+    name: str = "Tests"
     n_start: int = 1
-    parser: TestsParser = TestsParser()
-    verifier: TestsVerifier = TestsVerifier()
-    approximable: bool = True
+    approx_type = ApproxType.minimize
+
+    @staticmethod
+    def split(input: str) -> tuple[list[str], list[str]]:
+        raw_instance = []
+        raw_solution = []
+
+        for line in input:
+            if line[0] == 'i':
+                raw_instance.append(line)
+            elif line[0] == 's':
+                raw_solution.append(line)
+
+        return raw_instance, raw_solution
+    
+    @staticmethod
+    def parse_instance(instance: list[str], size: int) -> list[list[str]]:
+        parsed = [line.split(" ") for line in instance]
+        if not instance:
+            raise ValueError
+        if any(line[1] != '1' for line in instance):
+            raise ValueError
+        return parsed
+    
+    @staticmethod
+    def parse_solution(solution: list[str], size: int) -> list[list[str]]:
+        parsed = [line.split(" ") for line in solution]
+        if not parsed:
+            raise ValueError
+        if any(line[1] != '1' for line in solution):
+            raise ValueError
+        return parsed
+    
+    @staticmethod
+    def encode_instance(instance: list[list[str]]) -> str:
+        return "\n".join(" ".join(line) for line in instance)
+    
+    @staticmethod
+    def verify_solution(instance: list[str], size: int, solution: str) -> bool:
+        if any(line[2] != '1' for line in solution):
+            return False
+
+        return True
+    
+    @staticmethod
+    def solution_weight(instance: list[str], size: int, solutoion: str) -> float:
+        return len([line for line in solutoion if line[3] == '1'])
+
