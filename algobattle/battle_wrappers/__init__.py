ff6affc950811ea799fa01b862346ba164a1b951
Imogen <me@imogen.tech>
2022-03-04 17:45:27 +0100

    auto import all battle wrappers

--- algobattle/battle_wrappers/__init__.py
+++ algobattle/battle_wrappers/__init__.py
@@ -1,2 +1,6 @@
-from . import averaged  # type: ignore
-from . import iterated  # type: ignore
+from importlib import import_module
+from pathlib import Path
+
+for path in Path(__file__).resolve().parent.iterdir():
+    if not path.name[:2] == ("__") and path.name[-3:] == ".py":
+        import_module(f".{path.name[:-3]}", "algobattle.battle_wrappers")
