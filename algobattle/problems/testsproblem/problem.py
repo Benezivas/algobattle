7ffd5ba3a8367381a488d7c0039a11572ad459b6
Imogen <me@imogen.tech>
2022-03-04 17:14:17 +0100

    fix test problem

--- algobattle/problems/testsproblem/problem.py
+++ algobattle/problems/testsproblem/problem.py
@@ -18,7 +18,7 @@ class Tests(Problem[list[list[str]], list[str]]):
         raw_instance = []
         raw_solution = []
 
-        for line in input:
+        for line in input.splitlines():
             if line[0] == 'i':
                 raw_instance.append(line)
             elif line[0] == 's':
@@ -28,19 +28,19 @@ class Tests(Problem[list[list[str]], list[str]]):
     
     @staticmethod
     def parse_instance(instance: list[str], size: int) -> list[list[str]]:
-        parsed = [line.split(" ") for line in instance]
+        parsed = [line.strip().split() for line in instance if line.strip() != ""]
         if not instance:
             raise ValueError
-        if any(line[1] != '1' for line in instance):
+        if any(line[1] != '1' for line in parsed):
             raise ValueError
         return parsed
     
     @staticmethod
     def parse_solution(solution: list[str], size: int) -> list[list[str]]:
-        parsed = [line.split(" ") for line in solution]
+        parsed = [line.strip().split() for line in solution if line.strip() != ""]
         if not parsed:
             raise ValueError
-        if any(line[1] != '1' for line in solution):
+        if any(line[1] != '1' for line in parsed):
             raise ValueError
         return parsed
     
@@ -49,13 +49,13 @@ class Tests(Problem[list[list[str]], list[str]]):
         return "\n".join(" ".join(line) for line in instance)
     
     @staticmethod
-    def verify_solution(instance: list[str], size: int, solution: str) -> bool:
+    def verify_solution(instance: list[list[str]], size: int, solution: list[list[str]]) -> bool:
         if any(line[2] != '1' for line in solution):
             return False
 
         return True
     
     @staticmethod
-    def solution_weight(instance: list[str], size: int, solutoion: str) -> float:
+    def solution_weight(instance: list[list[str]], size: int, solutoion: list[list[str]]) -> float:
         return len([line for line in solutoion if line[3] == '1'])
 
