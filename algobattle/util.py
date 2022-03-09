470319c2b146006ebd399d5a42a2d80c7fd8956a
Imogen <me@imogen.tech>
2022-03-09 23:50:32 +0100

    update import_problem_from_path

--- algobattle/util.py
+++ algobattle/util.py
@@ -5,6 +5,7 @@ import importlib.util
 from pathlib import Path
 import sys
 from typing import Any
+from inspect import getmembers, isclass
 
 from algobattle.problem import Problem
 
@@ -12,31 +13,62 @@ from algobattle.problem import Problem
 logger = logging.getLogger('algobattle.util')
 
 
-def import_problem_from_path(problem_path: Path) -> Problem:
+def import_problem_from_path(path: Path) -> Problem:
     """Try to import and initialize a Problem object from a given path.
 
     Parameters
     ----------
-    problem_path : Path
+    path : Path
         Path in the file system to a problem folder.
 
     Returns
     -------
     Problem
-        Returns an object of the problem if successful, None otherwise.
+        Returns an object of the problem.
+    
+    Raises
+    ------
+    ValueError
+        If the given path does not point to a valid Problem.
+    RuntimeError
+        If some unexpected error occurs while importing the Problem.
     """
-    if not problem_path.exists():
-        logger.warning(f"Problem path '{problem_path}' does not exist in the file system!")
+    path = path.resolve()
+    if not path.exists():
+        logger.warning(f"Problem path '{path}' does not exist in the file system!")
         raise ValueError
     
+    if path.is_dir:
+        if (path / "__init__.py").is_file:
+            path /= "__init__.py"
+        elif (path / "problem.py").is_file:
+            path /= "problem.py"
+        else:
+            logger.warning(f"Problem path '{path}' points to a directory that doesn't contain a Problem.")
+            raise ValueError
+    
     try:
-        spec = importlib.util.spec_from_file_location("problem", problem_path / "__init__.py")
+        spec = importlib.util.spec_from_file_location("problem", path)
         assert spec is not None
         assert spec.loader is not None
-        Problem = importlib.util.module_from_spec(spec)
-        sys.modules[spec.name] = Problem
-        spec.loader.exec_module(Problem)
-        return Problem.Problem()
+        problem_module = importlib.util.module_from_spec(spec)
+        sys.modules[spec.name] = problem_module
+        spec.loader.exec_module(problem_module)
+
+        potential_problems = []
+        for _, obj in getmembers(problem_module):
+            # issubclass won't work here til 3.11
+            if isclass(obj) and Problem in obj.__bases__:
+                potential_problems.append(obj)
+        
+        if len(potential_problems) == 0:
+            logger.warning(f"Problem path '{path}' points to a module that doesn't contain a Problem.")
+            raise ValueError
+        if len(potential_problems) > 1:
+            logger.warning(f"Problem path '{path}' points to a module containing more than one Problem: {potential_problems}")
+            raise ValueError
+        
+        return potential_problems[0]()
 
     except Exception as e:
         logger.critical(f'Importing the given problem failed with the following exception: "{e}"')
