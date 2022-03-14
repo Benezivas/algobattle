3d6d9f2d58ba368d90aba6f4df53979e1d743596
Henri Lotze <info@henrilotze.de>
2022-03-14 12:36:57 +0100

    Add additional test for nonexistent problem path

--- tests/test_util.py
+++ tests/test_util.py
@@ -18,21 +18,20 @@ class Utiltests(unittest.TestCase):
     """Tests for the util functions."""
 
     def setUp(self) -> None:
-        self.Problem = importlib.import_module('algobattle.problems.testsproblem')
-        self.problem = self.Problem.Problem()
+        Problem = importlib.import_module('algobattle.problems.testsproblem')
+        self.problem = Problem.Problem()
         config_path = os.path.join(os.path.dirname(os.path.abspath(algobattle.__file__)), 'config', 'config.ini')
         self.config = ConfigParser()
         self.config.read(config_path)
         self.fight_handler = FightHandler(self.problem, self.config)
-        self.tests_path = self.Problem.__file__[:-12]  # remove /__init__.py
+        self.problem_path = Problem.__file__[:-12]  # remove /__init__.py
         self.rand_file_name = random.randint(0, 2 ** 80)
         while(os.path.exists(str(self.rand_file_name))):
             self.rand_file_name = random.randint(0, 2 ** 80)
 
     def test_import_problem_from_path_existing_path(self):
         """Importing works when importing a Problem from an existing path."""
-        tests_path = self.Problem.__file__[:-12]  # remove /__init__.py
-        self.assertIsNotNone(util.import_problem_from_path(tests_path))
+        self.assertIsNotNone(util.import_problem_from_path(self.problem_path))
 
     def test_import_problem_from_path_nonexistant_path(self):
         """An import fails if importing from a nonexistant path."""
@@ -52,27 +51,33 @@ class Utiltests(unittest.TestCase):
 
     def test_build_docker_container_timeout(self):
         """False is returned if building a container runs into a timeout."""
-        self.assertFalse(util.build_docker_container(os.path.join(self.tests_path, 'generator_build_timeout'),
+        self.assertFalse(util.build_docker_container(os.path.join(self.problem_path, 'generator_build_timeout'),
                                                      docker_tag='gen_bld_to',
                                                      timeout_build=0.5,
                                                      cache_docker_container=False))
 
     def test_build_docker_container_failed_build(self):
         """False is returned if building a docker container fails for any reason other than a timeout."""
-        self.assertFalse(util.build_docker_container(os.path.join(self.tests_path, 'generator_build_error'),
+        self.assertFalse(util.build_docker_container(os.path.join(self.problem_path, 'generator_build_error'),
                                                      docker_tag='gen_bld_err',
                                                      cache_docker_container=False))
 
     def test_build_docker_container_successful_build(self):
         """True is returned if a docker container builds successfully."""
-        self.assertTrue(util.build_docker_container(os.path.join(self.tests_path, 'generator'),
+        self.assertTrue(util.build_docker_container(os.path.join(self.problem_path, 'generator'),
                                                     docker_tag='gen_succ',
                                                     cache_docker_container=False))
+
+    def test_build_docker_container_nonexistant_path(self):
+        """False is returned if the path to the container does not exist in the file system."""
+        self.assertFalse(util.build_docker_container(os.path.join(self.problem_path, 'foobar'),
+                                                     docker_tag='foo_bar',
+                                                     cache_docker_container=False))
 
     def test_run_subprocess_timeout(self):
         """run_subprocess returns None on a subprocess timeout."""
         docker_tag = 'gen_to'
-        util.build_docker_container(os.path.join(self.tests_path, 'generator_timeout'),
+        util.build_docker_container(os.path.join(self.problem_path, 'generator_timeout'),
                                     docker_tag,
                                     cache_docker_container=False)
         run_command = self.fight_handler.base_run_command(self.fight_handler.space_generator) + [docker_tag]
@@ -82,7 +87,7 @@ class Utiltests(unittest.TestCase):
     def test_run_subprocess_execution_error(self):
         """run_subprocess returns None if an exception is thrown during execution of the subprocess."""
         docker_tag = 'gen_err'
-        util.build_docker_container(os.path.join(self.tests_path, 'generator_execution_error'),
+        util.build_docker_container(os.path.join(self.problem_path, 'generator_execution_error'),
                                     docker_tag,
                                     cache_docker_container=False)
         run_command = self.fight_handler.base_run_command(self.fight_handler.space_generator) + [docker_tag]
@@ -90,6 +95,7 @@ class Utiltests(unittest.TestCase):
         self.assertIsNone(raw_output)
 
     def test_update_nested_dict(self):
+        """A nested dict should be updated with information from another nested dict as expected."""
         dict_to_be_expanded = {0: 1, 1: {0: 0, 1: 0}, 2: 2}
         dict_expanding = {1: {1: 1}, 2: 1}
         self.assertEqual(util.update_nested_dict(dict_to_be_expanded, dict_expanding),
