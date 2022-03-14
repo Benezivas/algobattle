be8b4cc9079bf88cfefaa599513f67b2204a0f2e
Imogen <me@imogen.tech>
2022-03-14 15:46:42 +0100

    format problems

--- algobattle/problems/delaytest/problem.py
+++ algobattle/problems/delaytest/problem.py
@@ -3,36 +3,36 @@ import logging
 
 from algobattle.problem import ApproxType, Problem
 
-logger = logging.getLogger('algobattle.problems.delaytest')
+logger = logging.getLogger("algobattle.problems.delaytest")
 
 
 class Delaytest(Problem[None, None]):
     """Dummy Problem used for testing Docker delays."""
 
-    name = 'Runtime Delay Test'
+    name = "Runtime Delay Test"
     n_start: int = 1
     approx_type = ApproxType.maximize
 
     @staticmethod
     def split(input: str) -> tuple[list[str], list[str]]:
         return [], []
-    
+
     @staticmethod
     def parse_instance(instance: list[str], size: int) -> None:
         return None
-    
+
     @staticmethod
     def parse_solution(solution: list[str], size: int) -> None:
         return None
-    
+
     @staticmethod
     def encode_instance(instance: None) -> str:
         return ""
-    
+
     @staticmethod
     def verify_solution(instance: None, size: int, solution: None) -> bool:
         return True
-    
+
     @staticmethod
     def solution_weight(instance: None, size: int, solutoion: None) -> float:
         return 1
--- algobattle/problems/testsproblem/problem.py
+++ algobattle/problems/testsproblem/problem.py
@@ -3,7 +3,7 @@ import logging
 
 from algobattle.problem import Problem, ApproxType
 
-logger = logging.getLogger('algobattle.problems.testsproblem')
+logger = logging.getLogger("algobattle.problems.testsproblem")
 
 
 class Tests(Problem[list[list[str]], list[str]]):
@@ -19,43 +19,42 @@ class Tests(Problem[list[list[str]], list[str]]):
         raw_solution = []
 
         for line in input.splitlines():
-            if line[0] == 'i':
+            if line[0] == "i":
                 raw_instance.append(line)
-            elif line[0] == 's':
+            elif line[0] == "s":
                 raw_solution.append(line)
 
         return raw_instance, raw_solution
-    
+
     @staticmethod
     def parse_instance(instance: list[str], size: int) -> list[list[str]]:
         parsed = [line.strip().split() for line in instance if line.strip() != ""]
         if not instance:
             raise ValueError
-        if any(line[1] != '1' for line in parsed):
+        if any(line[1] != "1" for line in parsed):
             raise ValueError
         return parsed
-    
+
     @staticmethod
     def parse_solution(solution: list[str], size: int) -> list[list[str]]:
         parsed = [line.strip().split() for line in solution if line.strip() != ""]
         if not parsed:
             raise ValueError
-        if any(line[1] != '1' for line in parsed):
+        if any(line[1] != "1" for line in parsed):
             raise ValueError
         return parsed
-    
+
     @staticmethod
     def encode_instance(instance: list[list[str]]) -> str:
         return "\n".join(" ".join(line) for line in instance)
-    
+
     @staticmethod
     def verify_solution(instance: list[list[str]], size: int, solution: list[list[str]]) -> bool:
-        if any(line[2] != '1' for line in solution):
+        if any(line[2] != "1" for line in solution):
             return False
 
         return True
-    
+
     @staticmethod
     def solution_weight(instance: list[list[str]], size: int, solutoion: list[list[str]]) -> float:
-        return len([line for line in solutoion if line[3] == '1'])
-
+        return len([line for line in solutoion if line[3] == "1"])
