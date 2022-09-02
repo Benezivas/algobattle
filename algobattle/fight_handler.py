d5d30240556b56a69b86e8c6700277ab253c7745
Imogen <me@imogen.tech>
2022-09-02 12:27:50 +0200

    autoformat

--- algobattle/fight_handler.py
+++ algobattle/fight_handler.py
@@ -7,21 +7,21 @@ from algobattle.docker import DockerError
 from algobattle.team import Team
 from algobattle.problem import Problem
 
-logger = logging.getLogger('algobattle.fight_handler')
+logger = logging.getLogger("algobattle.fight_handler")
 
 
-class FightHandler():
+class FightHandler:
     """Class managing the execution of generators and solvers."""
 
     generating_team = None
     solving_team = None
 
     def __init__(self, problem: Problem, config: ConfigParser, runtime_overhead: float = 0) -> None:
-        self.timeout_generator = int(config['run_parameters']['timeout_generator']) + runtime_overhead
-        self.timeout_solver    = int(config['run_parameters']['timeout_solver']) + runtime_overhead
-        self.space_generator   = int(config['run_parameters']['space_generator'])
-        self.space_solver      = int(config['run_parameters']['space_solver'])
-        self.cpus              = int(config['run_parameters']['cpus'])
+        self.timeout_generator = int(config["run_parameters"]["timeout_generator"]) + runtime_overhead
+        self.timeout_solver = int(config["run_parameters"]["timeout_solver"]) + runtime_overhead
+        self.space_generator = int(config["run_parameters"]["space_generator"])
+        self.space_solver = int(config["run_parameters"]["space_solver"])
+        self.cpus = int(config["run_parameters"]["cpus"])
         self.problem = problem
 
     def set_roles(self, generating: Team, solving: Team) -> None:
@@ -40,12 +40,14 @@ class FightHandler():
     @staticmethod
     def team_roles_set(function: Callable) -> Callable:
         """Ensure that internal methods are only callable after the team roles have been set."""
+
         def wrapper(self, *args, **kwargs):
             if not self.generating_team or not self.solving_team:
-                logger.error('Generating or solving team have not been set!')
+                logger.error("Generating or solving team have not been set!")
                 return None
             else:
                 return function(self, *args, **kwargs)
+
         return wrapper
 
     @team_roles_set
@@ -74,9 +76,12 @@ class FightHandler():
         except RuntimeError:
             return 0
 
-        approximation_ratio = self.problem.verifier.calculate_approximation_ratio(instance, instance_size,
-                                                                                  generator_solution, solver_solution)
-        logger.info(f"Solver of group {self.solving_team} yields a valid solution with an approx. ratio of {approximation_ratio}.")
+        approximation_ratio = self.problem.verifier.calculate_approximation_ratio(
+            instance, instance_size, generator_solution, solver_solution
+        )
+        logger.info(
+            f"Solver of group {self.solving_team} yields a valid solution with an approx. ratio of {approximation_ratio}."
+        )
         return approximation_ratio
 
     @team_roles_set
@@ -95,7 +100,7 @@ class FightHandler():
         Any, Any
             If the validity checks pass, the (instance, solution) in whatever
             format that is specified
-        
+
         Raises
         ------
         RuntimeError
@@ -106,8 +111,10 @@ class FightHandler():
         logger.debug(f"Running generator of group {self.generating_team}...\n")
 
         try:
-            assert(self.generating_team is not None)
-            encoded_output = self.generating_team.generator.run(str(instance_size), self.timeout_generator, scaled_memory, self.cpus)
+            assert self.generating_team is not None
+            encoded_output = self.generating_team.generator.run(
+                str(instance_size), self.timeout_generator, scaled_memory, self.cpus
+            )
         except DockerError:
             logger.warning(f"Generator of team '{self.generating_team}' didn't run successfully!")
             raise RuntimeError
@@ -118,14 +125,14 @@ class FightHandler():
 
         raw_instance_with_solution = self.problem.parser.decode(encoded_output)
 
-        logger.debug('Checking generated instance and certificate...')
+        logger.debug("Checking generated instance and certificate...")
 
         raw_instance, raw_solution = self.problem.parser.split_into_instance_and_solution(raw_instance_with_solution)
-        instance                   = self.problem.parser.parse_instance(raw_instance, instance_size)
-        generator_solution         = self.problem.parser.parse_solution(raw_solution, instance_size)
+        instance = self.problem.parser.parse_instance(raw_instance, instance_size)
+        generator_solution = self.problem.parser.parse_solution(raw_solution, instance_size)
 
         if not self.problem.verifier.verify_semantics_of_instance(instance, instance_size):
-            logger.warning('Generator {} created a malformed instance!'.format(self.generating_team))
+            logger.warning("Generator {} created a malformed instance!".format(self.generating_team))
             raise RuntimeError
 
         if not self.problem.verifier.verify_semantics_of_solution(generator_solution, instance_size, True):
@@ -158,7 +165,7 @@ class FightHandler():
         any
             If the validity checks pass, solution in whatever
             format that is specified.
-        
+
         Raises
         ------
         RuntimeError
@@ -166,26 +173,32 @@ class FightHandler():
         """
         scaled_memory = self.problem.solver_memory_scaler(self.space_solver, instance_size)
         try:
-            assert(self.solving_team is not None)
-            encoded_output = self.solving_team.solver.run(self.problem.parser.encode(instance), self.timeout_solver, scaled_memory, self.cpus)
+            assert self.solving_team is not None
+            encoded_output = self.solving_team.solver.run(
+                self.problem.parser.encode(instance), self.timeout_solver, scaled_memory, self.cpus
+            )
         except DockerError:
             logger.warning(f"Solver of team '{self.solving_team}' didn't run successfully!")
             raise RuntimeError
-        
+
         if not encoded_output:
             logger.warning(f"No output was generated when running the solver of group {self.solving_team}!")
             raise ValueError
 
         raw_solver_solution = self.problem.parser.decode(encoded_output)
 
-        logger.debug('Checking validity of the solvers solution...')
+        logger.debug("Checking validity of the solvers solution...")
 
         solver_solution = self.problem.parser.parse_solution(raw_solver_solution, instance_size)
         if not self.problem.verifier.verify_semantics_of_solution(solver_solution, instance_size, True):
-            logger.warning(f"Solver of group {self.solving_team} created a malformed solution at instance size {instance_size}!")
+            logger.warning(
+                f"Solver of group {self.solving_team} created a malformed solution at instance size {instance_size}!"
+            )
             raise RuntimeError
         elif not self.problem.verifier.verify_solution_against_instance(instance, solver_solution, instance_size, False):
-            logger.warning(f"Solver of group {self.solving_team} yields an incorrect solution at instance size {instance_size}!")
+            logger.warning(
+                f"Solver of group {self.solving_team} yields an incorrect solution at instance size {instance_size}!"
+            )
             raise RuntimeError
 
         return solver_solution
