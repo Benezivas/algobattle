0cadf089f00c385445e11f1521ae26ca299a9655
Imogen <me@imogen.tech>
2022-10-03 15:50:16 +0200

    change observer and subject to new structure

--- algobattle/observer.py
+++ algobattle/observer.py
@@ -1,25 +1,26 @@
 """Observer class for an Observable pattern."""
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Any
-from algobattle.util import inherit_docs
+from typing import Callable, Generic, TypeVar
 
 
 class Observer(ABC):
     """The Observer interface declares the methods to receive updates from running matches."""
 
     @abstractmethod
-    def update(self, event: str, data: Any):
-        """Receive an update regarding `event` with `data`."""
+    def update(self, subject: Subject, update: str):
+        """Processes an update in the info of the subject."""
         raise NotImplementedError
 
 
 class Subject(ABC):
     """The Subject interface declares an easy way to update observers."""
 
-    def __init__(self) -> None:
+    def __init__(self, observer: Observer | None = None) -> None:
         super().__init__()
         self.observers: list[Observer] = []
+        if observer is not None:
+            self.attach(observer)
 
     def attach(self, observer: Observer):
         """Subscribes an observer to the updates of this subject."""
@@ -30,15 +31,28 @@ class Subject(ABC):
         if observer in self.observers:
             self.observers.remove(observer)
 
-    def notify(self, event: str, data: Any):
+    def notify(self, update: str = ""):
         """Updates all subscribed observers."""
         for observer in self.observers:
-            observer.update(event, data)
+            observer.update(self, update)
 
 
-class Passthrough(Subject, Observer, ABC):
-    """A class that is an observer and a subject and just passes notifications through."""
+T = TypeVar("T")
+class Notifying(Generic[T]):
+    def __init__(self, callback: Callable[[str], None] | None = None) -> None:
+        super().__init__()
+        self.callback = callback
 
-    @inherit_docs
-    def update(self, event: str, data: Any):
-        self.notify(event, data)
+    def __set_name__(self, obj: Subject, name: str) -> None:
+        self.name = name
+        self.private_name = "_" + name
+        if self.callback is None:
+            self.callback = obj.notify
+
+    def __get__(self, obj: Subject, objtype=None) -> T:
+        return getattr(obj, self.private_name)
+    
+    def __set__(self, obj: Subject, value: T) -> None:
+        setattr(obj, self.private_name, value)
+        if self.callback is not None:
+            self.callback(self.name)
