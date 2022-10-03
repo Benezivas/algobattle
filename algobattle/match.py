a08a4d9e389ef95d8eeb35ce56e88a7e91b768a0
Imogen <me@imogen.tech>
2022-10-03 16:22:04 +0200

    update match to use new observer/subject

--- algobattle/match.py
+++ algobattle/match.py
@@ -6,25 +6,21 @@ import logging
 from prettytable import PrettyTable, DOUBLE_BORDER
 
 from algobattle.battle_wrapper import BattleWrapper
+from algobattle.observer import Observer, Subject
 from algobattle.team import Matchup, Team
 from algobattle.fight_handler import FightHandler
-from algobattle.observer import Passthrough
 
 logger = logging.getLogger('algobattle.match')
 
 
 @dataclass
-class Match(Passthrough):
-    """Central managing class for an algorithmic battle."""
+class MatchInfo:
+    """Class specifying all the parameters to run a match."""
 
     fight_handler: FightHandler
     battle_wrapper: BattleWrapper
     teams: list[Team]
     rounds: int = 5
-
-    def __post_init__(self):
-        super().__init__()
-        self.battle_wrapper.attach(self)
 
     @property
     def grouped_matchups(self) -> list[tuple[Matchup, Matchup]]:
@@ -42,70 +38,70 @@ class Match(Passthrough):
         else:
             return [m for pair in self.grouped_matchups for m in pair]
 
-    def run(self) -> Match.Result:
-        """Match entry point, executes fights between all teams."""
-        result = Match.Result(self)
-        self.notify("match_result", result)
+    def run_match(self, observer: Observer | None) -> MatchResult:
+        """Executes the match with the specified parameters."""
+        result = MatchResult(self, observer)
         for matchup in self.matchups:
             for i in range(self.rounds):
                 logger.info("#" * 20 + f"  Running Round {i+1}/{self.rounds}  " + "#" * 20)
                 battle_result = self.battle_wrapper.run_round(self.fight_handler, matchup)
                 result[matchup].append(battle_result)
-                self.notify("match_result", result)
+                result.notify()
         return result
 
-    class Result(dict[Matchup, list[BattleWrapper.Result]]):
-        """The Result of a whole Match."""
+class MatchResult(Subject, dict[Matchup, list[BattleWrapper.Result]]):
+    """The Result of a whole Match."""
 
-        def __init__(self, match: Match):
-            self.match = match
-            super().__init__({m: [] for m in match.matchups})
+    def __init__(self, match: MatchInfo, observer: Observer | None = None):
+        self.match = match
+        dict.__init__(self, {m: [] for m in match.matchups})
+        Subject.__init__(self, observer)
 
-        def calculate_points(self, achievable_points: int) -> dict[Team, float]:
-            """Calculate the number of points each team scored.
+    def calculate_points(self, achievable_points: int) -> dict[Team, float]:
+        """Calculate the number of points each team scored.
 
-            Each pair of teams fights for the achievable points among one another.
-            These achievable points are split over all rounds.
-            """
-            if len(self.match.teams) == 1:
-                return {self.match.teams[0]: achievable_points}
+        Each pair of teams fights for the achievable points among one another.
+        These achievable points are split over all rounds.
+        """
+        if len(self.match.teams) == 1:
+            return {self.match.teams[0]: achievable_points}
 
-            if any(not 0 <= len(results) <= self.match.rounds for results in self.values()):
-                raise ValueError
+        if any(not 0 <= len(results) <= self.match.rounds for results in self.values()):
+            raise ValueError
 
-            points = {team: 0. for team in self.match.teams}
-            if self.match.rounds == 0:
-                return points
-            points_per_round = round(achievable_points / self.match.rounds, 1)
-
-            for home_matchup, away_matchup in self.match.grouped_matchups:
-                for home_res, away_res in zip(self[home_matchup], self[away_matchup]):
-                    total_score = home_res.score + away_res.score
-                    if total_score == 0:
-                        # Default values for proportions, assuming no team manages to solve anything
-                        home_ratio = 0.5
-                        away_ratio = 0.5
-                    else:
-                        home_ratio = home_res.score / total_score
-                        away_ratio = away_res.score / total_score
-
-                    points[home_matchup.solver] += round(points_per_round * home_ratio, 1)
-                    points[away_matchup.solver] += round(points_per_round * away_ratio, 1)
-
+        points = {team: 0. for team in self.match.teams}
+        if self.match.rounds == 0:
             return points
+        points_per_round = round(achievable_points / self.match.rounds, 1)
 
-        def __str__(self) -> str:
-            table = PrettyTable(field_names=["GEN", "SOL", *range(self.match.rounds), "AVG"], min_width=5)
-            table.set_style(DOUBLE_BORDER)
-            table.align["AVG"] = "r"
-            for i in range(self.match.rounds):
-                table.align[str(i)] = "r"
+        for home_matchup, away_matchup in self.match.grouped_matchups:
+            for home_res, away_res in zip(self[home_matchup], self[away_matchup]):
+                total_score = home_res.score + away_res.score
+                if total_score == 0:
+                    # Default values for proportions, assuming no team manages to solve anything
+                    home_ratio = 0.5
+                    away_ratio = 0.5
+                else:
+                    home_ratio = home_res.score / total_score
+                    away_ratio = away_res.score / total_score
 
-            for matchup, results in self.items():
-                if not 0 <= len(results) <= self.match.rounds:
-                    raise RuntimeError
-                padding = [""] * (self.match.rounds - len(results))
-                average = "" if len(results) == 0 else results[1].format_score(sum(r.score for r in results) / len(results))
-                table.add_row([matchup.generator, matchup.solver, *results, *padding, average])
+                points[home_matchup.solver] += round(points_per_round * home_ratio, 1)
+                points[away_matchup.solver] += round(points_per_round * away_ratio, 1)
 
-            return f"Battle Type: {self.match.battle_wrapper}\n{table}"
+        return points
+
+    def __str__(self) -> str:
+        table = PrettyTable(field_names=["GEN", "SOL", *range(self.match.rounds), "AVG"], min_width=5)
+        table.set_style(DOUBLE_BORDER)
+        table.align["AVG"] = "r"
+        for i in range(self.match.rounds):
+            table.align[str(i)] = "r"
+
+        for matchup, results in self.items():
+            if not 0 <= len(results) <= self.match.rounds:
+                raise RuntimeError
+            padding = [""] * (self.match.rounds - len(results))
+            average = "" if len(results) == 0 else results[1].format_score(sum(r.score for r in results) / len(results))
+            table.add_row([matchup.generator, matchup.solver, *results, *padding, average])
+
+        return f"Battle Type: {self.match.battle_wrapper}\n{table}"
