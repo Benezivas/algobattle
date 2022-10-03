9949417446e970b8d642e5a61a1aee29d714d6a3
Imogen <me@imogen.tech>
2022-10-03 18:24:35 +0200

    update observer interface

--- algobattle/observer.py
+++ algobattle/observer.py
@@ -1,7 +1,7 @@
 """Observer class for an Observable pattern."""
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Callable, Generic, TypeVar
+from typing import Any
 
 
 class Observer(ABC):
@@ -37,23 +37,6 @@ class Subject(ABC):
         for observer in self.observers:
             observer.update(self, event)
 
-
-T = TypeVar("T")
-class Notifying(Generic[T]):
-    def __init__(self, callback: Callable[[str], None] | None = None) -> None:
-        super().__init__()
-        self.callback = callback
-
-    def __set_name__(self, obj: Subject, name: str) -> None:
-        self.name = name
-        self.private_name = "_" + name
-        if self.callback is None:
-            self.callback = obj.notify
-
-    def __get__(self, obj: Subject, objtype=None) -> T:
-        return getattr(obj, self.private_name)
-    
-    def __set__(self, obj: Subject, value: T) -> None:
-        setattr(obj, self.private_name, value)
-        if self.callback is not None:
-            self.callback(self.name)
+    def __setattr__(self, name: str, value: Any) -> None:
+        super().__setattr__(name, value)
+        self.notify(name)
