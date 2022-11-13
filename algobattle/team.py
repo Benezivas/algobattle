641e9867eb2086230ef4bcfcf9321c58ee291776
Imogen <me@imogen.tech>
2022-11-13 23:11:37 +0100

    update Team class

--- algobattle/team.py
+++ algobattle/team.py
@@ -1,7 +1,7 @@
 """Team class, stores necessary information about a Team, such as their associated solver and generator."""
 from __future__ import annotations
 from pathlib import Path
-from dataclasses import dataclass
+from dataclasses import InitVar, dataclass
 from typing import Iterator
 
 from algobattle.docker_util import DockerError, Image
@@ -41,59 +41,28 @@ class TeamInfo:
         return Team(name, generator, solver)
 
 
-
+@dataclass
 class Team:
     """Team class responsible for holding basic information of a specific team."""
 
-    def __init__(
-        self,
-        team_name: str,
-        generator: Path | Image,
-        solver: Path | Image,
-        timeout_build: float | None = None,
-    ) -> None:
-        """Creates a team object and builds the necessary docker containers.
+    name: str
+    generator: Image
+    solver: Image
+    _cleanup_generator: bool = False
+    _cleanup_solver: bool = False
 
-        Parameters
-        ----------
-        team_name : str
-            Name of the team, must be globally unique!
-        generator_path : Path | Image
-            Path to a folder containing a Dockerfile that will be used for the generator, or already built image.
-        solver_path : Path | Image
-            Path to a folder containing a Dockerfile that will be used for the solver, or already built image.
-        timeout_build : float | None
-            Timeout for building the containers, `None` means they will have unlimited time, by default None.
+    def __post_init__(self) -> None:
+        """Creates a team object.
 
         Raises
         ------
         ValueError
-            _description_
+            If the team name is already in use.
         """
         super().__init__()
-        team_name = team_name.replace(" ", "_").lower()  # Lower case needed for docker tag created from name
-        if team_name in _team_names:
+        self.name = self.name.replace(" ", "_").lower()  # Lower case needed for docker tag created from name
+        if self.name in _team_names:
             raise ValueError
-        self.name = team_name
-        built_generator = None
-        try:
-            self._built_generator = isinstance(generator, Path)
-            if isinstance(generator, Path):
-                self.generator = Image(generator, f"generator-{self}", f"generator for team {self}",
-                                       timeout=timeout_build)
-                built_generator = self.generator
-            else:
-                self.generator = generator
-            self._built_solver = isinstance(solver, Path)
-            if isinstance(solver, Path):
-                self.solver = Image(solver, f"solver-{self}", f"solver for team {self}", timeout_build)
-            else:
-                self.solver = solver
-        except DockerError:
-            if built_generator is not None:
-                built_generator.remove()
-            raise
-        _team_names.add(team_name)
 
     def __str__(self) -> str:
         return self.name
@@ -109,9 +78,9 @@ class Team:
 
     def cleanup(self) -> None:
         """Removes the built docker images."""
-        if self._built_generator:
+        if self._cleanup_generator:
             self.generator.remove()
-        if self._built_solver:
+        if self._cleanup_solver:
             self.solver.remove()
         _team_names.remove(self.name)
 
