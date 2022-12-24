1caf12de6dfae06b3e0346ea6a5d95d8d72c4010
Imogen <me@imogen.tech>
2022-12-24 10:43:25 +0100

    wip

--- algobattle/problem.py
+++ algobattle/problem.py
@@ -1,35 +1,32 @@
 """Abstract base class for problem classes used in concrete problem implementations."""
-from typing import Protocol
+from abc import ABC
+from dataclasses import dataclass
+from typing import Type
+from pydantic import BaseModel
 
-from algobattle.parser import Parser
-from algobattle.verifier import Verifier
+class Instance(ABC, BaseModel):
+    """Represents a specific instance of a problem."""
+
+    @classmethod
+    def parse(cls, source: )
 
 
-class Problem(Protocol):
-    """Problem Class, bundling together the verifier and parser of a problem.
+class Solution(BaseModel):
+    """Represents a potential solution to an instance of a problem."""
 
-    Enforces the necessary attribute n_start which is the smallest iteration
-    size for a problem as well as a flag indicating whether a problem is
-    usable in an approximation setting.
-    """
+
+@dataclass(kw_only=True)
+class Problem:
+    """Dataclass specifying what a problem's instances and solutions look like."""
 
     name: str
-    n_start: int
-    """Lowest value on which a battle should be executed."""
-    parser: Parser
-    """Parser object for the corresponding problem."""
-    verifier: Verifier
-    """Verifier object for the corresponding problem."""
-    approximable: bool
-    """Boolean flag indicating whether a problem can have an approximate solution."""
-
-    def generator_memory_scaler(self, memory, instance_size):
-        """Method that scales the amount of memory of the generator in relation to the given instance size."""
-        return memory
-
-    def solver_memory_scaler(self, memory, instance_size):
-        """Method that scales the amount of memory of the solver in relation to the given instance size."""
-        return memory
+    """Name of the problem."""
+    n_start: int = 1
+    """Lowest value of n valid for this problem"""
+    instance_type: Type[Instance]
+    """Type of the instances of this problem."""
+    solution_type: Type[Solution]
+    """Type of the solutions of this problem."""
 
     def __str__(self) -> str:
         return self.name
