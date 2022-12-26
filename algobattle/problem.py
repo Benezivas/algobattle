33216cb513f0a2fd8398cdc34c077fad9af13735
Imogen <me@imogen.tech>
2022-12-26 15:13:46 +0100

    add parse method

--- algobattle/problem.py
+++ algobattle/problem.py
@@ -1,14 +1,38 @@
 """Abstract base class for problem classes used in concrete problem implementations."""
 from abc import ABC
 from dataclasses import dataclass
-from typing import Type
+from io import BytesIO
+from tarfile import TarFile
+from typing import Type, TypeVar
 from pydantic import BaseModel
+
+
+_Self = TypeVar("_Self", bound="Instance")
+
+
+class ProblemError(Exception):
+    """Parent class of all exceptions related to the problem module."""
+    pass
+
+
+class ContainerError(ProblemError):
+    """Raised when the container returned malformed data."""
+    pass
+
 
 class Instance(ABC, BaseModel):
     """Represents a specific instance of a problem."""
 
     @classmethod
-    def parse(cls, source: )
+    def parse(cls: Type[_Self], archive: bytes) -> _Self:
+        """Parses the generator output into a problem instance."""
+        with BytesIO(initial_bytes=archive) as fh, TarFile.open(fileobj=fh, mode="r") as tar:
+            try:
+                output_file = tar.extractfile("output")
+                assert output_file is not None
+            except (KeyError, AssertionError):
+                raise ContainerError
+            return cls.parse_raw(output_file.read())
 
 
 class Solution(BaseModel):
@@ -21,8 +45,8 @@ class Problem:
 
     name: str
     """Name of the problem."""
-    n_start: int = 1
-    """Lowest value of n valid for this problem"""
+    start_size: int = 1
+    """Smallest valid size for this problem"""
     instance_type: Type[Instance]
     """Type of the instances of this problem."""
     solution_type: Type[Solution]
