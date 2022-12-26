baaa1b43062d95c3e77b2627e7c95b507fdbc93b
Imogen <me@imogen.tech>
2022-12-27 00:11:39 +0100

    add Instance method default impls

--- algobattle/problem.py
+++ algobattle/problem.py
@@ -2,9 +2,11 @@
 from abc import ABC
 from dataclasses import dataclass
 from io import BytesIO
-from tarfile import TarFile
-from typing import Type, TypeVar
+from pathlib import Path
+from typing import Annotated, Any, TypeVar, get_type_hints
 from pydantic import BaseModel
+
+from algobattle.util import FileArchive
 
 
 _Self = TypeVar("_Self", bound="Instance")
@@ -29,15 +31,37 @@ class Instance(ABC, BaseModel):
     """Represents a specific instance of a problem."""
 
     @classmethod
-    def parse(cls: Type[_Self], archive: bytes) -> _Self:
-        """Parses the generator output into a problem instance."""
-        with BytesIO(initial_bytes=archive) as fh, TarFile.open(fileobj=fh, mode="r") as tar:
-            try:
-                output_file = tar.extractfile("output")
-                assert output_file is not None
-            except (KeyError, AssertionError):
-                raise ContainerError
-            return cls.parse_raw(output_file.read())
+    def parse(cls: type[_Self], source: FileArchive) -> _Self:
+        """Parses the generator output into a problem instance.
+        
+        The default implementation expects the object to be json encoded at a file 'instance.json'.
+        """
+        try:
+            return cls.parse_raw(source[Path("instance.json")])
+        except KeyError:
+            raise ContainerError
+
+    def verify_semantics(self):
+        """Validates that the instance is semantically correct."""
+        pass
+
+    def encode(self, **kwargs: dict[str, Any]) -> FileArchive:
+        """Encodes the instance into files so it can be passed to docker containers.
+        
+        By default a single file `instance.json` is generated and attributes annotated with :cls:`Hidden` are ignored.
+        Battle wrappers may specify additional arguments via `kwargs` to fine tune the info passed to containers.
+        """
+        return FileArchive({Path("instance.json"): self.json(exclude=self._excludes()).encode()})
+
+    @classmethod
+    def _excludes(cls) -> dict[str | int, Any]:
+        excludes = {}
+        for name, annotation in get_type_hints(cls, include_extras=True).items():
+            if hasattr(annotation, "__metadata__") and Hidden in annotation.__metadata__:
+                excludes[name] = True
+            elif issubclass(annotation, Instance):
+                excludes[name] = annotation._excludes()
+        return excludes
 
 
 class Solution(BaseModel):
@@ -52,9 +76,9 @@ class Problem:
     """Name of the problem."""
     start_size: int = 1
     """Smallest valid size for this problem"""
-    instance_type: Type[Instance]
+    instance_type: type[Instance]
     """Type of the instances of this problem."""
-    solution_type: Type[Solution]
+    solution_type: type[Solution]
     """Type of the solutions of this problem."""
 
     def __str__(self) -> str:
