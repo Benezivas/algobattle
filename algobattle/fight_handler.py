3c5a415e60186f5a3b4149ae2b110a8dc1edd209
Imogen <me@imogen.tech>
2022-12-30 14:03:02 +0100

    wip update fight handler to work with new problem

--- algobattle/fight_handler.py
+++ algobattle/fight_handler.py
@@ -1,89 +1,125 @@
 """Class managing the execution of generators and solvers."""
-from dataclasses import KW_ONLY, dataclass
+from __future__ import annotations
+from dataclasses import dataclass, field
 import logging
-from typing import Any, Tuple
+from typing import Any, Generic, TypeGuard, TypeVar
 
 from algobattle.docker_util import DockerError
 from algobattle.team import Matchup, Team
-from algobattle.problem import Problem
+from algobattle.problem import Instance, Problem, ProblemData, Solution
+from algobattle.util import TempDir
 
 logger = logging.getLogger("algobattle.fight_handler")
 
+
+_T = TypeVar("_T", bound=ProblemData)
+_Instance, _Solution = TypeVar("_Instance", bound=Instance), TypeVar("_Solution", bound=Solution[Any])
+
+
 @dataclass
-class FightHandler:
+class Result(Generic[_T]):
+    """Result of a single generator or solver execution."""
+    data: _T
+    time: float
+
+
+class FightError(Exception):
+    """Raised when a team's program doesn't generate valid output."""
+
+
+class InvalidOutput(FightError):
+    """Indicates that the created instance or solution is invalid."""
+    pass
+
+
+@dataclass
+class FightResult(Generic[_Instance, _Solution]):
+    """The result of a single fight."""
+    score: float
+    generator: Result[_Instance] | FightError
+    solver: Result[_Solution] | FightError | None
+
+def generator_success(result: FightResult[_Instance, _Solution]) -> TypeGuard[GeneratorSuccess[_Instance, _Solution]]:
+    """Checks whether both programs ran successfully."""
+    return isinstance(result.generator, Result)
+
+def is_success(result: FightResult[_Instance, _Solution]) -> TypeGuard[Success[_Instance, _Solution]]:
+    """Checks whether both programs ran successfully."""
+    return isinstance(result.solver, Result)
+
+@dataclass
+class GeneratorFailure(FightResult[_Instance, _Solution]):
+    """The result of a fight where the generator didn't run successfully."""
+    generator: FightError
+    solver: None = field(default=None, init=False)
+
+
+@dataclass
+class GeneratorSuccess(FightResult[_Instance, _Solution]):
+    """The result of a fight where the generator ran successfully."""
+    generator: Result[_Instance]
+    solver: Result[_Solution] | FightError
+
+
+@dataclass
+class Success(FightResult[_Instance, _Solution]):
+    """The result of a fight were both programs ran successfully."""
+    generator: Result[_Instance]
+    solver: Result[_Solution]
+
+
+@dataclass(kw_only=True)
+class FightHandler(Generic[_Instance, _Solution]):
     """Class managing the execution of generators and solvers."""
 
-    problem: Problem
-    _: KW_ONLY
+    problem: Problem[_Instance, _Solution]
+    matchup: Matchup
     timeout_generator: float | None = None
     timeout_solver: float | None = None
     space_generator: int | None = None
     space_solver: int | None = None
     cpus: int = 1
 
-    def fight(self, matchup: Matchup, instance_size: int) -> float:
-        """Execute a single fight of a battle between a given generator and solver for a given instance size.
-
-        Parameters
-        ----------
-        instance_size : int
-            The instance size, expected to be a positive int.
-
-        Returns
-        -------
-        float
-            Returns the approximation ratio of the solver against
-            the generator (1 if optimal, 0 if failed, >=1 if the
-            generator solution is optimal).
-        """
+    def fight(self, size: int, timeout_generator: float | None = ..., space_genrator: int | None = ..., timeout_solver: float | None = ..., space_solver: int | None = ..., cpus: int = ...) -> FightResult[_Instance, _Solution]:
+        """Execute a single fight of a battle, running the generator and solver and handling any errors gracefully."""
         try:
-            instance, generator_solution = self._run_generator(matchup.generator, instance_size)
-        except RuntimeError:
-            return 1.0
+            gen_result = self.generate(size=size, timeout=timeout_generator, space=space_genrator, cpus=cpus)
+        except FightError as e:
+            return GeneratorFailure(score=1, generator=e)
 
         try:
-            solver_solution = self._run_solver(matchup.solver, instance_size, instance)
-        except RuntimeError:
-            return 0
+            sol_result = self.solve(size=size, timeout=timeout_solver, space=space_solver, cpus=cpus)
+        except FightError as e:
+            return GeneratorSuccess(score=1, generator=gen_result, solver=e)
 
-        approximation_ratio = self.problem.verifier.calculate_approximation_ratio(
-            instance, instance_size, generator_solution, solver_solution
-        )
-        logger.info(
-            f"Solver of group {matchup.generator} yields a valid solution with an approx. ratio of {approximation_ratio}."
-        )
-        return approximation_ratio
+        score = self.problem.calculate_score(gen_result.data, sol_result.data)
+        logger.info(f"Solver of group {self.matchup.generator} yields a valid solution with an approx. ratio of {score}.")
+        return Success(score, gen_result, sol_result)
 
-    def _run_generator(self, team: Team, instance_size: int) -> Tuple[Any, Any]:
-        """Execute the generator of `team` and check the validity of the generated output.
+    def generate(self, size: int, timeout: float | None = ..., space: int | None = ..., cpus: int = ...) -> Result[_Instance]:
+        """Execute the generator and process its output."""
+        logger.debug(f"Running generator of team {self.matchup.generator}.")
+        if timeout is ellipsis:
+            timeout = self.timeout_generator
+        if space is ellipsis:
+            space = self.space_generator
+        if cpus is ellipsis:
+            cpus = self.cpus
 
-        If the validity checks pass, return the instance and the certificate solution.
+        with TempDir() as input, TempDir() as output:
+            with open(input / "size") as f:
+                f.write(str(size))
 
-        Parameters
-        ----------
-        instance_size : int
-            The instance size, expected to be a positive int.
+            try:
+                runtime = self.matchup.generator.generator.run(input, output, timeout=timeout, memory=space, cpus=cpus)
+            except ExecutionError as e:
+                logger.warning(f"Generator of team '{self.matchup.generator}' crashed!")
+                return Failure("execution", time=e.time)
+            except DockerError:
+                logger.warning(f"Generator of team '{self.matchup.generator}' couldn't be executed successfully!")
+                return Failure("execution", time=-1)
 
-        Returns
-        -------
-        Any, Any
-            If the validity checks pass, the (instance, solution) in whatever
-            format that is specified
-
-        Raises
-        ------
-        RuntimeError
-            If the container doesn't run successfully or any of the checks don't pass
-        """
-        scaled_memory = self.problem.generator_memory_scaler(self.space_generator, instance_size)
-
-        logger.debug(f"Running generator of group {team}...\n")
-
-        try:
-            encoded_output = team.generator.run(str(instance_size), self.timeout_generator, scaled_memory, self.cpus)
-        except DockerError:
-            logger.warning(f"Generator of team '{team}' didn't run successfully!")
-            raise RuntimeError
+            
 
         if not encoded_output:
             logger.warning(f"No output was generated when running the generator group {team}!")
@@ -115,7 +151,7 @@ class FightHandler:
 
         return instance, generator_solution
 
-    def _run_solver(self, team: Team, instance_size: int, instance: Any) -> Any:
+    def solve(self, size: int, timeout: float | None = ..., space: int | None = ..., cpus: int = ...) -> Result[_Solution]:
         """Execute the solver of `team` and check the validity of the generated output.
 
         If the validity checks pass, return the solver solution.
