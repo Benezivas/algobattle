c233509899758561a5f47dc94accfaaea23becee
Imogen <me@imogen.tech>
2023-01-01 20:29:24 +0100

    update testsproblem

--- tests/testsproblem/problem.py
+++ tests/testsproblem/problem.py
@@ -1,18 +1,28 @@
 """Problem class built for tests."""
 import logging
+from typing import Annotated, SupportsFloat
 
-from algobattle.problem import Problem
-from .parser import TestsParser
-from .verifier import TestsVerifier
+from algobattle.problem import ProblemModel, SolutionModel
+from algobattle.util import Hidden
 
 logger = logging.getLogger('algobattle.problems.testsproblem')
 
 
-class Tests(Problem):
+class Tests(ProblemModel):
     """Artificial problem used for tests."""
 
-    name = 'Tests'
-    n_start = 1
-    parser = TestsParser()
-    verifier = TestsVerifier()
-    approximable = True
+    class Solution(SolutionModel):
+        difference: int
+
+    name = "Tests"
+    min_size = 0
+
+    first_val: int
+    second_val: int
+    solution: Annotated[Solution, Hidden()]
+
+    def check_semantics(self, size: int) -> bool:
+        return self.first_val + self.second_val <= size
+
+    def calculate_score(self, solution: Solution, size: int) -> SupportsFloat:
+        return self.first_val - self.second_val == solution.difference
