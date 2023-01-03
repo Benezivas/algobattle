40f2ffa1b8cd908e52d6802ca3e4f584be3b55d3
Imogen <me@imogen.tech>
2023-01-03 21:19:34 +0100

    add graph problem base classes

--- algobattle/problem.py
+++ algobattle/problem.py
@@ -4,7 +4,9 @@ import importlib.util
 import logging
 import sys
 from pathlib import Path
-from typing import Any, ClassVar, SupportsFloat, Self
+from typing import Any, ClassVar, SupportsFloat, Self, Generic, TypeVar
+from pydantic import Field
+from pydantic.generics import GenericModel
 from algobattle.util import CustomEncodable, BaseModel, Role
 
 logger = logging.getLogger("algobattle.problem")
@@ -127,3 +129,63 @@ class SolutionModel(BaseModel, Problem.Solution, ABC):
         fields = {
             "filename": {"exclude": True},
         }
+
+
+class DirectedGraph(ProblemModel):
+    """Base class for problems on directed graphs."""
+
+    num_vertices: int = Field(ge=0, le=2**63-1)
+    edges: list[tuple[int, int]] = Field(ge=0, le=2**63-1)
+
+    def check_semantics(self, size: int) -> bool:
+        return (
+            self.num_vertices <= size
+            and all(u < self.num_vertices and v < self.num_vertices for u, v in self.edges)
+            and len(self.edges) == len(set(self.edges))
+        )
+
+
+class UndirectedGraph(DirectedGraph):
+    """Base class for problems on undirected graphs."""
+
+    def check_semantics(self, size: int) -> bool:
+        if not super().check_semantics(size):
+            return False
+        edges = set(self.edges)
+        return (
+            all(u != v for u, v in edges)
+            and all((v, u) not in edges for u, v in edges)
+        )
+
+
+Weight = TypeVar("Weight")
+
+
+class EdgeWeights(GenericModel, Generic[Weight]):
+    """Mixin for graphs with weighted edges."""
+
+    edge_weights: list[Weight]
+
+    def check_semantics(self, size: int) -> bool:
+        assert isinstance(self, DirectedGraph)
+        as_parent = super()
+        if isinstance(as_parent, Problem):
+            if not as_parent.check_semantics(size):
+                return False
+
+        return len(self.edge_weights) == len(self.edges)
+
+
+class VertexWeights(GenericModel, Generic[Weight]):
+    """Mixin for graphs with weighted vertices."""
+
+    vertex_weights: list[Weight]
+
+    def check_semantics(self, size: int) -> bool:
+        assert isinstance(self, DirectedGraph)
+        as_parent = super()
+        if isinstance(as_parent, Problem):
+            if not as_parent.check_semantics(size):
+                return False
+
+        return len(self.vertex_weights) == self.num_vertices
