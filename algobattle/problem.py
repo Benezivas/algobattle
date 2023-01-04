a84cf58428ea6106f64a05aaabaa7fcf5c5a1a50
Imogen <me@imogen.tech>
2023-01-04 17:04:24 +0100

    move OptimizationProblem functionality into the base Problem class

--- algobattle/problem.py
+++ algobattle/problem.py
@@ -4,16 +4,17 @@ import importlib.util
 import logging
 import sys
 from pathlib import Path
-from typing import Any, ClassVar, Literal, SupportsFloat, Self, Generic, TypeAlias, TypeVar
+from typing import Any, ClassVar, Literal, Protocol, SupportsFloat, Self, Generic, TypeAlias, TypeVar, runtime_checkable
 from pydantic import Field
 from pydantic.generics import GenericModel
-from algobattle.util import CustomEncodable, EncodableModel, Role, inherit_docs
+from algobattle.util import CustomEncodable, EncodableModel, Role
 
 logger = logging.getLogger("algobattle.problem")
 
 
 _Problem: TypeAlias = Any
 _Solution: TypeAlias = Any
+Solution: TypeAlias
 
 
 class ProblemError(Exception):
@@ -26,6 +27,18 @@ class ContainerError(ProblemError):
     pass
 
 
+@runtime_checkable
+class Scored(Protocol):
+    """A solution with an associated score."""
+
+    direction: ClassVar[Literal["minimize", "maximize"]]
+
+    @abstractmethod
+    def score(self, size: int, instance: _Problem) -> float:
+        """Calculate the score of this solution for the given problem instance."""
+        raise NotImplementedError  
+
+
 class Problem(CustomEncodable, ABC):
     """Problem base class."""
 
@@ -34,6 +47,12 @@ class Problem(CustomEncodable, ABC):
 
     min_size: ClassVar[int] = 0
     """Minimum size of valid instances of this problem."""
+
+    with_solution: ClassVar[bool] = False
+    """Whether an instance of this problem also comes with a solution."""
+
+    solution: "Solution" | None
+    """The generator's solution for this instance."""
 
     @classmethod
     @abstractmethod
@@ -50,33 +69,31 @@ class Problem(CustomEncodable, ABC):
         """Validates that the parsed instance is semantically correct."""
         return True
 
-    class Solution(CustomEncodable, ABC):
-        """A proposed solution for an instance of this problem."""
-
-        @classmethod
-        @abstractmethod
-        def decode(cls: type[Self], source_dir: Path, size: int) -> Self:
-            """Parses the container output into problem data."""
-            raise NotImplementedError
-
-        @abstractmethod
-        def encode(self, target_dir: Path, size: int, team: Role) -> None:
-            """Encodes the solution into files that can be passed to docker containers."""
-            raise NotImplementedError
-
-        def check_semantics(self, size: int, instance: _Problem) -> bool:
-            """Validates that the parsed solution is semantically correct."""
-            return True
-
-    @abstractmethod
     def calculate_score(self, solution: _Solution, size: int) -> SupportsFloat:
         """Calculates how well a solution solves this problem instance.
         
-        Return values are clamped to fall inside [0, 1].
-        A value of 0 indicating that the solver failed completely
+        Return values are should be inside [0, 1].
+        With a value of 0 indicating that the solver failed completely
         and 1 that it solved the instance perfectly.
         """
-        raise NotImplementedError
+        if isinstance(self.solution, self.Solution) and isinstance(self.solution, Scored):
+            # we have a default impl if the problem comes with a generator solution and the solution type implements the ScoredSolution protocol
+            # we can't check data protocol subclass relationships at runtime so we need to check if the solution is an instance instead
+            # we know that the generator's and solver's solutions are of the same type so we don't need to check both
+            assert isinstance(solution, Scored)
+            gen_score = self.solution.score(size, self)
+            if gen_score == 0:
+                return 1
+            sol_score = solution.score(size, self)
+            if sol_score == 0:
+                return 0
+
+            if self.solution.direction == "minimize":
+                return gen_score / sol_score
+            else:
+                return sol_score / gen_score 
+        else:
+            raise NotImplementedError
 
     @staticmethod
     def import_from_path(path: Path) -> type["Problem"]:
@@ -109,11 +126,29 @@ class Problem(CustomEncodable, ABC):
             logger.critical(f"Importing the given problem failed with the following exception: {e}")
             raise ValueError from e
 
+    class Solution(CustomEncodable, ABC):
+        """A proposed solution for an instance of this problem."""
+
+        @classmethod
+        @abstractmethod
+        def decode(cls: type[Self], source_dir: Path, size: int) -> Self:
+            """Parses the container output into problem data."""
+            raise NotImplementedError
+
+        @abstractmethod
+        def encode(self, target_dir: Path, size: int, team: Role) -> None:
+            """Encodes the solution into files that can be passed to docker containers."""
+            raise NotImplementedError
+
+        def check_semantics(self, size: int, instance: _Problem) -> bool:
+            """Validates that the parsed solution is semantically correct."""
+            return True
+
 
 class ProblemModel(EncodableModel, Problem, ABC):
     """A Problem that can easily be parsed to/from a json file."""
 
-    filename = "instance.json"
+    filename: ClassVar[str] = "instance.json"
 
     class Config:
         fields = {
@@ -121,57 +156,20 @@ class ProblemModel(EncodableModel, Problem, ABC):
             "name": {"exclude": True},
             "min_size": {"exclude": True},
             "Solution": {"exclude": True},
+            "has_solution": {"exclude": True},
+            "solution": {"exclude": True},
         }
 
 
 class SolutionModel(EncodableModel, Problem.Solution, ABC):
     """A solution that can easily be parsed to/from a json file."""
 
-    filename = "solution.json"
+    filename: ClassVar[str] = "solution.json"
 
     class Config:
         fields = {
             "filename": {"exclude": True},
         }
-
-
-class OptimiztionSolution(SolutionModel, ABC):
-    """A solution with an associated score."""
-
-    direction: ClassVar[Literal["minimize", "maximize"]] = "maximize"
-
-    @abstractmethod
-    def score(self, size: int, instance: _Problem) -> float:
-        """Calculate the score of this solution for the given problem instance."""
-        raise NotImplementedError    
-
-
-Solution: TypeAlias = OptimiztionSolution
-
-
-class Optimization(ProblemModel, ABC):
-    """A problem mixin where each instance comes with a solution and the goal is to provide a better scoring solution."""
-
-    solution: "Solution" = Field(hidden="solver")
-
-    def check_semantics(self, size: int) -> bool:
-        return self.solution.check_semantics(size, self)
-
-    @inherit_docs
-    def calculate_score(self, solution: _Solution, size: int) -> SupportsFloat:
-        assert issubclass(self.Solution, OptimiztionSolution)
-        assert isinstance(solution, self.Solution)
-        gen_score = self.solution.score(size, self)
-        if gen_score == 0:
-            return 1
-        sol_score = solution.score(size, self)
-        if sol_score == 0:
-            return 0
-
-        if self.Solution.direction == "minimize":
-            return gen_score / sol_score
-        else:
-            return sol_score / gen_score    
 
 
 class DirectedGraph(ProblemModel):
