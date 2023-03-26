24d8065ab3e0340a09c0d5dd45ee72a23b0bdcc8
Imogen <me@imogen.tech>
2023-03-26 15:12:19 +0200

    remove rounds

--- algobattle/match.py
+++ algobattle/match.py
@@ -18,7 +18,6 @@ class MatchConfig(BaseModel):
     """Parameters determining the match execution."""
 
     battle_type: type[Battle] = Iterated
-    rounds: int = 5
     points: int = 100
 
     @validator("battle_type", pre=True)
@@ -47,7 +46,7 @@ class Match(Subject):
         teams: TeamHandler,
         observer: Observer | None = None,
     ) -> None:
-        self.results: dict[Matchup, list[Battle]] = {}
+        self.results: dict[Matchup, Battle] = {}
         self.config = config
         self.battle_config = battle_config
         self.problem = problem
@@ -66,16 +65,13 @@ class Match(Subject):
         """Executes the match with the specified parameters."""
         result = cls(config, battle_config, problem, teams, observer)
         for matchup in teams.matchups:
-            result.results[matchup] = []
-            for i in range(config.rounds):
-                logger.info("#" * 20 + f"  Running Round {i+1}/{config.rounds}  " + "#" * 20)
-                battle = config.battle_type(observer=observer)
-                result.results[matchup].append(battle)
-                try:
-                    battle.run_battle(matchup.generator.generator, matchup.solver.solver, battle_config, problem.min_size)
-                except Exception as e:
-                    logger.critical(f"Unhandeled error during execution of battle!\n{e}")
-                result.notify("match")
+            battle = config.battle_type(observer=observer)
+            result.results[matchup] = battle
+            try:
+                battle.run_battle(matchup.generator.generator, matchup.solver.solver, battle_config, problem.min_size)
+            except Exception as e:
+                logger.critical(f"Unhandeled error during execution of battle!\n{e}")
+            result.notify("match")
         return result
 
     def calculate_points(self) -> dict[str, float]:
@@ -90,51 +86,40 @@ class Match(Subject):
         if len(self.teams.active) == 1:
             return {self.teams.active[0].name: achievable_points}
 
-        if any(not 0 <= len(results) <= self.config.rounds for results in self.results.values()):
-            raise ValueError
-
         points = {team.name: 0.0 for team in self.teams.active + self.teams.excluded}
-        if self.config.rounds == 0:
-            return points
-        points_per_battle = round(achievable_points / ((len(self.teams.active) - 1) * self.config.rounds), 1)
+        points_per_battle = round(achievable_points / (len(self.teams.active) - 1), 1)
 
         for home_matchup, away_matchup in self.teams.grouped_matchups:
             home_team: Team = getattr(home_matchup, self.config.battle_type.scoring_team)
             away_team: Team = getattr(away_matchup, self.config.battle_type.scoring_team)
-            for home_res, away_res in zip(self.results[home_matchup], self.results[away_matchup]):
-                total_score = home_res.score() + away_res.score()
-                if total_score == 0:
-                    # Default values for proportions, assuming no team manages to solve anything
-                    home_ratio = 0.5
-                    away_ratio = 0.5
-                else:
-                    home_ratio = home_res.score() / total_score
-                    away_ratio = away_res.score() / total_score
+            home_res = self.results[home_matchup]
+            away_res = self.results[away_matchup]
+            total_score = home_res.score() + away_res.score()
+            if total_score == 0:
+                # Default values for proportions, assuming no team manages to solve anything
+                home_ratio = 0.5
+                away_ratio = 0.5
+            else:
+                home_ratio = home_res.score() / total_score
+                away_ratio = away_res.score() / total_score
 
-                points[home_team.name] += round(points_per_battle * home_ratio, 1)
-                points[away_team.name] += round(points_per_battle * away_ratio, 1)
+            points[home_team.name] += round(points_per_battle * home_ratio, 1)
+            points[away_team.name] += round(points_per_battle * away_ratio, 1)
 
         # we need to also add the points each team would have gotten fighting the excluded teams
         # each active team would have had one set of battles against each excluded team
         for team in self.teams.active:
-            points[team.name] += points_per_battle * len(self.teams.excluded) * self.config.rounds
+            points[team.name] += points_per_battle * len(self.teams.excluded)
 
         return points
 
     def display(self) -> str:
         """Formats the match data into a table that can be printed to the terminal."""
-        table = PrettyTable(field_names=["GEN", "SOL", *range(1, self.config.rounds + 1), "AVG"], min_width=5)
+        table = PrettyTable(field_names=["Generator", "Solver", "Result"], min_width=5)
         table.set_style(DOUBLE_BORDER)
-        table.align["AVG"] = "r"
-        for i in range(1, self.config.rounds + 1):
-            table.align[str(i)] = "r"
+        table.align["Result"] = "r"
 
-        for matchup, results in self.results.items():
-            if not 0 <= len(results) <= self.config.rounds:
-                raise RuntimeError
-            padding = [""] * (self.config.rounds - len(results))
-            average = "" if len(results) == 0 else results[0].format_score(sum(r.score() for r in results) / len(results))
-            results = [r.format_score(r.score()) for r in results]
-            table.add_row([str(matchup.generator), str(matchup.solver), *results, *padding, average])
+        for matchup, result in self.results.items():
+            table.add_row([str(matchup.generator), str(matchup.solver), result.format_score(result.score())])
 
         return f"Battle Type: {self.config.battle_type.name()}\n{table}"
